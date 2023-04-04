f7874b74e8828f267cef81a37989ecb88308ea6b
Ian Hancock <northroadian@gmail.com>
2023-04-04 12:36:04 -0500

    search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import CharactersList from "./pages/CharactersList";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Character from "./pages/Character";
+import Search from './pages/Search'
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<CharactersList />}></Route>
+        <Route path="/search" element={<Search />}></Route>
         <Route path="/:id" element={<Character />}></Route>
       </Routes>
     </div>
---
+++ src/pages/Search.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
+const GET_CHARACTER_LOCATIONS = gql`
+  query GetCharacterLocations($name: String!) {
+    characters(filter: { name: $name }) {
+      results {
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
+
+export default function Search() {
+  const [name, setName] = useState();
+  const [getLocations, { loading, error, data, called }] = useLazyQuery(
+    GET_CHARACTER_LOCATIONS,
+    { variables: { name } }
+  );
+  console.log({ loading, error, data, called });
+  if (loading) return <div>...riding spinners riding spinners</div>;
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+
+      <button onClick={() => getLocations()}>Search</button>
+      {data && (
+        <ul>
+          {data.characters.results.map((character) => {
+            return <li>{character.location.name}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
