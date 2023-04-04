dada5a50a02cdebe58b8fac14ebaf9cd9a95c556
Ian Hancock <northroadian@gmail.com>
2023-04-04 10:34:16 -0500

    route params

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { gql } from '@apollo/client'
+import React from "react";
 import { useCharacter } from "./useCharacter";
+import { useParams } from "react-router";
 
-export default function Character(){
-    const {error,data,loading} = useCharacter(4)
+export default function Character() {
+  const {id} = useParams()
+  const { error, data, loading } = useCharacter(id);
 
-    console.log({error,data,loading})
-    if(error) return <div>oh no this is not good</div>
-    if (loading) return <div>......loading......</div>
-    return(
-        <div>
-           <img  src={data.character.image}/>
-           <div>{data.character.name}</div>
-           <div>{data.character.episode.map(show=>{
-            return(
-                <>
-                <div>{show.episode}</div>
-                <div>{show.name}</div>
-                </>
-            )
-           })}
-           </div>
-        </div>
-    )
+  console.log({ error, data, loading });
+  if (error) return <div>oh no this is not good</div>;
+  if (loading) return <div>......loading......</div>;
+  return (
+    <div>
+      <img src={data.character.image} />
+      <div>{data.character.name}</div>
+      <div>
+        {data.character.episode.map((episode) => {
+          return (
+            <div>
+              <div>{episode.name}</div>
+              <div>{episode.episode}COOL</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useCharacters } from "./useCharacters";
 import './CharactersList.css'
-
+import {Link} from 'react-router-dom'
 export default function CharactersList() {
 const {error,data,loading} = useCharacters()
 
 if(loading) return <div>riding spinners....</div>
 if (error) return <div>oh no</div>
   return <div className="character-list">
-    {data.characters.results.map(character =>{
+    {data.characters.results.map((character) =>{
         return (
-            <div key={character.id}>
+        
+            <Link to={`/${character.id}`}>
+                <div>{character.id}</div>
         <img src ={character.image}/>
         <div>{character.name}</div>
-                </div>
+                </Link>
           )
     })}
   </div>;
